22662ac4b1759cea799550adfbb619b597852788
Bablu Mehta <babloomehta895012@gmail.com>
2024-02-20 12:39:03 +0530

    barChart is created successfully with styles as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import "./App.css";
+import BarChart from "./components/BarChart";
+import { UserData } from "./data";
 
 function App() {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "User Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: ["red", "green", "blue", "yellow", "grey"],
+        borderColor: 'black',
+        borderWidth: 2
+      },
+    ],
+  });
 
   return (
     <>
-      <div></div>
+      <div style={{ width: 700 }}>
+        <BarChart chartData={userData} />
+      </div>
     </>
   );
 }
