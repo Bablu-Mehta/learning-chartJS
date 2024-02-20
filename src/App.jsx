dd633010c0d95d2bd63ac8820bb68fea4e7e2020
Bablu Mehta <babloomehta895012@gmail.com>
2024-02-20 12:45:04 +0530

    LineChart is alos added successfully

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import BarChart from "./components/BarChart";
 import { UserData } from "./data";
+import LineChart from "./components/LineChart";
 
 function App() {
   const [userData, setUserData] = useState({
@@ -11,16 +12,20 @@ function App() {
         label: "User Gained",
         data: UserData.map((data) => data.userGain),
         backgroundColor: ["red", "green", "blue", "yellow", "grey"],
-        borderColor: 'black',
-        borderWidth: 2
+        borderColor: "black",
+        borderWidth: 2,
       },
     ],
   });
 
   return (
     <>
-      <div style={{ width: 700 }}>
+      <div style={{ width: 500, display: "inline-block", margin: "2rem" }}>
         <BarChart chartData={userData} />
+      </div>
+
+      <div style={{ width: 500, display: "inline-block", margin: "2rem" }}>
+        <LineChart chartData={userData} />
       </div>
     </>
   );
